e4b9429817ea1c47fbf471af6b2a04c78f456da6
Sean Philippi <s.philippi@rsitex.com>
2020-03-07 17:08:10 -0600

    enzyme setup

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -5,8 +5,7 @@ import App from '../components/App';
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
-    // looks inside the div and checks to see if the CommentBox is in there
-    // console.log(div.innerHTML)
-    expect(div.innerHTML).toContain('Hi there');
+    expect(div.innerHTML).toContain('Comment Box');
+
     ReactDOM.unmountComponentAtNode(div);
 });
