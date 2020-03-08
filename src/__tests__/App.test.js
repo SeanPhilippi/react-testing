ab053c291597745080718e1c487ba0d684892901
Sean Philippi <s.philippi@rsitex.com>
2020-03-08 14:47:17 -0500

    changed relative paths to absolute paths with jsconfig.json

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../components/App';
-import CommentBox from '../components/CommentBox';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
-it('renders without crashing', () => {
-    // component
-    const wrapped = shallow(<App />);
+// component
+const wrapped = shallow(<App />);
 
+it('shows a comment box', () => {
     expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+    expect(wrapped.find(CommentList).length).toEqual(1);
 });
