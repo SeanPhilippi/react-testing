f39198524fdeb8350ef5032578af3c9ae31b7420
Sean Philippi <s.philippi@rsitex.com>
2020-03-15 17:30:57 -0500

    testing for new comment, testing for clearing comment upon submit

--- src/__tests__/CommentBox.test.js
+++ src/__tests__/CommentBox.test.js
@@ -21,4 +21,18 @@ it('has a text area that users can type in', () => {
     wrapped.find('textarea').simulate('change', { // pass in string of native html event name
         target: { value: 'new comment' } // mock event object
     });
+    // force React to re-render component, since it is async
+    wrapped.update();
+    // making sure the value prop of textarea is now set to the new comment
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
+
+it('clears the textarea upon submit', () => {
+    wrapped.find('textarea').simulate('change', { // pass in string of native html event name
+        target: { value: 'new comment' } // mock event object
+    });
+    wrapped.update();
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
 });
